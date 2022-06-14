4f251562ee43e5404c20b271f7f48db3e78150f2
zachliu <zqliu643@outlook.com>
2022-06-14 20:12:03 +0800

    全部提交

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 import React from 'react'
+require('icons/tag.svg');
+require('icons/money2.svg');
+require('icons/chart.svg');
+
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -10,7 +14,14 @@ const NavWrapper = styled.nav`
     > li{
       width: 33.3333%;
       text-align:center;
-      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 4px 0;
+      .icon{
+        width: 30px;
+        height: 30px;
+      }
     }
   }
 `;
@@ -20,12 +31,21 @@ const Nav=()=>{
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#tag"/>
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money2"/>
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart"/>
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
