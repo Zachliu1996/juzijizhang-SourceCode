827551828015742061aafc991b03893c2c522f55
zachliu <zqliu643@outlook.com>
2022-06-15 01:45:58 +0800

    创建views目录

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,32 @@
 import React from 'react';
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect
-} from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav'
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display:flex;
-  flex-direction: column;
-  
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+import Tags from './views/Tags'
+import Money from './views/Money'
+import Statistics from './views/Statistic'
+import NoMatch from './views/NoMatch'
 
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from="/" to="/money"/>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
+      <Switch>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        <Redirect exact from="/" to="/money"/>
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+      </Switch>
     </Router>
   );
-}
-
-function NoMatch() {
-  return (
-    <div>页面不存在，你丫输错地址了吧！</div>
-  );
-}
-
-function Statistics() {
-  return <h2>统计页面</h2>;
-}
-
-function Tags() {
-  return <h2>标签页面</h2>;
-}
-
-function Money() {
-  return <h2>记账页面</h2>;
 }
 
 export default App;
